abece73cdc1d52efd1238d448db7f5c0bd280363
leemingee <leemingee1995@gmail.com>
2018-02-26 17:18:27 -0500

    Initial the envs and hello world for opencv

---
+++ initial.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+'''
+filename = '/Users/wanrenzhifeng/Git-leemingee/PoodleKFC/Data/train/images/0001.jpg'
+img = cv2.imread(filename, 0)
+cv2.imshow('image', img)
+cv2.waitKey(1000)
+cv2.destroyAllWindows()
+'''
+
+'''
+
+filename = '/Users/wanrenzhifeng/Git-leemingee/PoodleKFC/Data/train/images/0001.jpg'
+img = cv2.imread(filename, 0)
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+'''
